8dda0b1c6558029cef3362afb8fa78ef053ef051
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-13 13:15:55 -0300

    nu router paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
+import 'package:proj/screens/favorites_screen.dart';
 import 'package:proj/screens/home_screen.dart';
-
+import 'package:proj/screens/login_screen.dart';
+import 'package:proj/screens/package_details_screen.dart';
+import 'package:proj/screens/payment_screen.dart';
+import 'package:proj/screens/producer_details_screen.dart';
+import 'package:proj/screens/profile_screen.dart';
+import 'package:proj/screens/signup_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +19,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Montserrat',
-        ),
-        home: Nuvigator.routes(initialRoute: 'home', routes: [
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+      ),
+      home: Nuvigator.routes(
+        initialRoute: 'home',
+        routes: [
           NuRouteBuilder(path: 'home', builder: (_, __, ___) => HomeScreen()),
-        ]));
+          NuRouteBuilder(path: 'login', builder: (_, __, ___) => LoginScreen()),
+          NuRouteBuilder(
+              path: 'sign-up', builder: (_, __, ___) => SignupScreen()),
+          NuRouteBuilder(
+              path: 'favorites', builder: (_, __, ___) => FavoritesScreen()),
+          NuRouteBuilder(
+              path: 'profile', builder: (_, __, ___) => ProfileScreen()),
+          NuRouteBuilder(
+              path: 'payment', builder: (_, __, ___) => PaymentScreen()),
+          NuRouteBuilder(
+            path: 'producer-details',
+            builder: (_, __, NuRouteSettings args) {
+              final Producer producer = args.rawParameters['producer'];
+              return ProducerDetailsScreen(producer: producer);
+            },
+          ),
+          NuRouteBuilder(
+            path: 'producer-details',
+            builder: (_, __, NuRouteSettings args) {
+              final Producer producer = args.rawParameters['producer'];
+              final Package package = args.rawParameters['package'];
+              return PackageDetailsScreen(package: package, producer: producer);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
