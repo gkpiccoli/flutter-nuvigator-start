589dc17a0c3d9ecd57e1d24d4fb5ffc97d5ad793
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-14 19:07:17 -0300

    favorites route

--- lib/routes/favorites_route.dart
+++ lib/routes/favorites_route.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nuvigator/next.dart';
 
-
 import '../screens/favorites_screen.dart';
 
 class FavoritesRoute extends NuRoute {
   @override
   Widget build(BuildContext context, NuRouteSettings<Object> settings) {
     return FavoritesScreen(
-      ],
+      onLoginClick: () => nuvigator.open('login'),
     );
   }
 
